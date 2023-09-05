ed8ebef35460b32eb4164373fd5120e5d8b5802f
Edurb23 <rm551987@fiap.com.br>
2023-09-05 16:19:15 -0300

    Adicionando pasta produtos e home

---
+++ src/Produtos/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Produto = () =>{
+    return(
+        <main>
+            <h1>Produtos</h1>
+        </main>
+    )
+}
+
+export default Produto
---
+++ src/home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+    return(
+        <main>
+            <H>HOME</H>
+        </main>
+    )
+}
+
+export default Home
