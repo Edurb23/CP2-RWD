3464448248dbc8b00ad00b92953e18319a916f5f
Edurb23 <rm551987@fiap.com.br>
2023-09-05 16:28:15 -0300

    alterando o mainjsx e adicionando o metodoCreateBrowser

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element:<Produto/>
+      },
+      {
+        path: "/editar/produtos/:id",
+        element: <EditarProduto/>
+      },
+      
+    ]
+    
+  }
+])
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
