05b95018694851f96e7859c768926476fc7ba007
Edurb23 <rm551987@fiap.com.br>
2023-09-05 16:21:53 -0300

    adicionando editarproduto e error

---
+++ src/editarProdutos/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditarProdutos = () => {
+    return(
+        <main>
+            <h1>
+                Editando o produto
+            </h1>
+        </main>
+    )
+}
---
+++ src/error/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Error = () => {
+    return(
+        <>
+        <h1>Error 404 - Página não encontrada</h1>
+        </>
+    )
+}
