36ed8ca7918390e9a692a5da79241772604cb49b
Edurb23 <rm551987@fiap.com.br>
2023-09-05 16:26:30 -0300

    importanto para o appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/home'
+import Produto from './components/produtos'
+import Error from './components/error'
+import EditarProduto from './components/editarProdutos'
 
 function App() {
   const [count, setCount] = useState(0)
